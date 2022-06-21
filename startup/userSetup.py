f43b964dc277d08a4d523e680b8d7a2c118ca94a
Neil Rubie <neilrubie@yahoo.ca>
2022-06-21 11:29:51 -0400

    Adds userSetup to establish shared directory upon maya launch

--- startup/userSetup.py
+++ startup/userSetup.py
@@ -1,2 +1,10 @@
 """userSetup for Studio Library."""
 
+# SSE Imports
+from asset_manager.api import am
+import studiolibrary
+
+# Set a shared project library everyone can view and modify.
+shared_path = am.get_animation_library_shared_root()
+libraries = [{"name":"Shared", "path":shared_path, "default":True}]
+studiolibrary.setLibraries(libraries)
